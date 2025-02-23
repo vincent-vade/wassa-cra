157fdf6e1d0dd72cbabab279ff59da34227cff2e
vincentvade <vincentvad@gmail.com>
2025-02-23 11:58:50 +0100

    add new ui for login

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,4 +1,11 @@
+import "@mantine/core/styles.css";
 import "~/styles/globals.css";
+
+import {
+	type MantineColorsTuple,
+	MantineProvider,
+	createTheme,
+} from "@mantine/core";
 import type { AppProps } from "next/app";
 
 import { AuthProvider } from "~/context/AuthContext";
@@ -6,25 +13,35 @@ import { ToasterProvider } from "~/context/ToastContext";
 
 import Toaster from "~/components/Toaster";
 
-import "@mantine/core/styles.css";
-
-// import "../styles/table.css";
-
-import { MantineProvider, createTheme } from "@mantine/core";
+const base: MantineColorsTuple = [
+	"#eeedff",
+	"#d9d7fc",
+	"#afacf1",
+	"#837ee6",
+	"#5e57dd",
+	"#463ed8",
+	"#3932d7",
+	"#2b25bf",
+	"#2420ac",
+	"#1a1a98",
+];
 
 const theme = createTheme({
-	/** Put your mantine theme override here */
+	colors: {
+		base,
+	},
+	primaryColor: "base",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-    <ToasterProvider>
+		<ToasterProvider>
 			<MantineProvider theme={theme}>
 				<AuthProvider>
 					<Component {...pageProps} />
 					<Toaster />
 				</AuthProvider>
 			</MantineProvider>
-      </ToasterProvider>
+		</ToasterProvider>
 	);
 }
--- apps/web/src/pages/login/index.tsx
+++ apps/web/src/pages/login/index.tsx
@@ -1,7 +1,15 @@
-// import "./login.css";
-
-import { Box, Button, Group, TextInput } from "@mantine/core";
+import {
+	Button,
+	Container,
+	Group,
+	Paper,
+	PasswordInput,
+	Stack,
+	TextInput,
+	Title,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
+import Link from "next/link";
 
 import { useAuth } from "~/context/AuthContext";
 
@@ -11,56 +19,67 @@ const LoginPage = () => {
 			email: "",
 			password: "",
 		},
+		validateInputOnBlur: true,
 		validate: {
 			email: (value: string) =>
-				/^\S+@\S+$/.test(value) ? null : "Invalid email",
+				!/^\S+@\S+$/.test(value) ? "Invalid email" : null,
+			password: (val: string) =>
+				val.length <= 6
+					? "Password should include at least 6 characters"
+					: null,
 		},
 	});
 
 	const auth = useAuth();
 
 	return (
-		<Group grow gap="lg" justify="center" h="100vh">
-			<Box>
-				<h1>Login</h1>
-			</Box>
-			<Box p="lg">
+		<Container mt="25vh" size={480}>
+			<Paper radius="md" p="xl" withBorder shadow="sm">
+				<Title order={3} mb="lg">
+					Se connecter à Wassa CRA
+				</Title>
+
 				<form
 					onSubmit={form.onSubmit((values) => {
 						auth?.login(values.email, values.password);
 					})}
 				>
-					<TextInput
-						label="Email"
-						name="email"
-						size="lg"
-						key={form.key("email")}
-						withAsterisk
-						mb="lg"
-						{...form.getInputProps("email")}
-					/>
-					<TextInput
-						label="Password"
-						name="password"
-						type="password"
-						size="lg"
-						key={form.key("password")}
-						withAsterisk
-						mb="lg"
-						{...form.getInputProps("password")}
-					/>
-
-					<Button
-						variant="filled"
-						type="submit"
-						size="lg"
-						style={{ float: "right" }}
-					>
-						Login
-					</Button>
+					<Stack>
+						<TextInput
+							label="Email"
+							name="email"
+							key={form.key("email")}
+							withAsterisk
+							size="md"
+							color="base"
+							variant="filled"
+							{...form.getInputProps("email")}
+						/>
+						<PasswordInput
+							label="Password"
+							name="password"
+							key={form.key("password")}
+							withAsterisk
+							size="md"
+							variant="filled"
+							{...form.getInputProps("password")}
+						/>
+					</Stack>
+					<Group justify="space-between" mt="xl">
+						<Button
+							component={Link}
+							href="/reset-password"
+							variant="transparent"
+						>
+							Mot de passe oublié ?
+						</Button>
+						<Button variant="filled" type="submit" size="md">
+							Se connecter
+						</Button>
+					</Group>
 				</form>
-			</Box>
-		</Group>
+			</Paper>
+		</Container>
 	);
 };
 
